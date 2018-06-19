e3c67d123bfb504c0524221049dad47b82c6cfed
Jeff Escalante <gh.je@mailhero.io>
2018-06-18 23:07:54 -0400

    fix a huge bug with prefixed attributes

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const parser = require('..')
 const util = require('util')
 const test = require('ava')
 
-test('basic', (t) => {
+test('basic', t => {
   const str = '<p class="wow">hello world</p>'
   const out = parser(str)
   t.truthy(out[0].type === 'tag')
@@ -15,8 +15,9 @@ test('basic', (t) => {
   t.truthy(out[0].location.col === 1)
 })
 
-test('head/body', (t) => {
-  const str = '<!doctype html><html><head><link rel="stylesheet" href="/style.css"></head><body><p>hi!</p></body></html>'
+test('head/body', t => {
+  const str =
+    '<!doctype html><html><head><link rel="stylesheet" href="/style.css"></head><body><p>hi!</p></body></html>'
   const out = parser(str)
   t.truthy(out[0].type === 'text')
   t.truthy(out[0].content.match(/!DOCTYPE/))
@@ -27,7 +28,7 @@ test('head/body', (t) => {
   t.truthy(out[1].content[1].content[0].name === 'p')
 })
 
-test('text', (t) => {
+test('text', t => {
   const str = 'hi there'
   const out = parser(str)
   t.truthy(out[0].type === 'text')
@@ -36,7 +37,7 @@ test('text', (t) => {
   t.truthy(out[0].location.col === 1)
 })
 
-test('tag', (t) => {
+test('tag', t => {
   const str = '<p>hi</p>'
   const out = parser(str)
   t.truthy(out[0].type === 'tag')
@@ -48,7 +49,7 @@ test('tag', (t) => {
   t.truthy(out[0].location.col === 1)
 })
 
-test('comment', (t) => {
+test('comment', t => {
   const str = '<!-- hi -->'
   const out = parser(str)
   t.truthy(out[0].type === 'comment')
@@ -57,7 +58,7 @@ test('comment', (t) => {
   t.truthy(out[0].location.col === 1)
 })
 
-test('nesting', (t) => {
+test('nesting', t => {
   const str = '<l1><l2><l3>3text</l3>2text</l2></l1>'
   const out = parser(str)
   t.truthy(out[0].name === 'l1')
@@ -67,8 +68,9 @@ test('nesting', (t) => {
   t.truthy(out[0].content[0].content[1].content === '2text')
 })
 
-test('attributes', (t) => {
-  const str = '<p id="wow" class="wow bow" data-foo="bar" @snap="crackle">hi</p>'
+test('attributes', t => {
+  const str =
+    '<p id="wow" class="wow bow" data-foo="bar" @snap="crackle">hi</p>'
   const out = parser(str)
   t.truthy(out[0].attrs.id[0].content === 'wow')
   t.truthy(out[0].attrs.class[0].content === 'wow bow')
@@ -77,33 +79,41 @@ test('attributes', (t) => {
 })
 
 // parse5 ignores second copies of any attribute
-test('multiple matching attributes', (t) => {
+test('multiple matching attributes', t => {
   const str = '<p class="one" class="two">hi</p>'
   const out = parser(str)
   t.truthy(Object.keys(out[0].attrs).length === 1)
   t.truthy(out[0].attrs.class[0].content === 'one')
 })
 
-test('boolean attributes', (t) => {
+test('boolean attributes', t => {
   const str = '<input type="checkbox" selected>hi</input>'
   const out = parser(str)
   t.truthy(out[0].attrs.type[0].content === 'checkbox')
   t.truthy(out[0].attrs.selected[0].content === '')
 })
 
-test('filename option', (t) => {
+test('filename option', t => {
   const str = '<p class="foo">hello</p>'
   const out = parser(str, { filename: 'index.html' })
   t.truthy(out[0].attrs.class[0].location.filename === 'index.html')
   t.truthy(out[0].location.filename === 'index.html')
 })
 
-test('svg parse', (t) => {
-  const str = '<svg width="178px" height="297px" viewBox="0 0 178 297" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>Slice 1</title><desc>Created with Sketch.</desc><defs></defs><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><circle id="Oval" stroke="#979797" fill="#D8D8D8" cx="45" cy="252" r="44"></circle><circle id="Oval-Copy" stroke="#979797" fill="#D8D8D8" cx="133" cy="252" r="44"></circle><ellipse id="Oval-2" stroke="#979797" fill="#D8D8D8" cx="89.5" cy="113" rx="29.5" ry="112"></ellipse></g></svg>'
+test('svg parse', t => {
+  const str =
+    '<svg width="178px" height="297px" viewBox="0 0 178 297" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>Slice 1</title><desc>Created with Sketch.</desc><defs></defs><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><circle id="Oval" stroke="#979797" fill="#D8D8D8" cx="45" cy="252" r="44"></circle><circle id="Oval-Copy" stroke="#979797" fill="#D8D8D8" cx="133" cy="252" r="44"></circle><ellipse id="Oval-2" stroke="#979797" fill="#D8D8D8" cx="89.5" cy="113" rx="29.5" ry="112"></ellipse></g></svg>'
   const out = parser(str, { filename: 'index.html' })
   t.truthy(out[0].name === 'svg')
 })
 
-function inspect (tree) { // eslint-disable-line
+test('prefixed attributes', t => {
+  const str = '<svg><image xlink:href="#foo" /></svg>'
+  const out = parser(str, { filename: 'index.html' })
+  t.truthy(out[0].content[0].attrs['xlink:href'])
+})
+
+function inspect(tree) {
+  // eslint-disable-line
   console.log(util.inspect(tree, { depth: null, showHidden: true }))
 }
